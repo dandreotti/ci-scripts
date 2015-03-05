cb523ae0ce579959888718baca7c499958f9db64
dandreotti <daniele.andreotti@cnaf.infn.it>
2015-03-05 17:14:44 +0100

    Added parameter for selecting a specific branch of the testsuite

--- docker/storm-deployment-test/server.sh
+++ docker/storm-deployment-test/server.sh
@@ -26,6 +26,7 @@ PLATFORM="${PLATFORM:-SL6}"
 STORM_REPO="${STORM_REPO:-http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_storm_develop_SL6/lastSuccessfulBuild/artifact/storm_develop_sl6.repo}"
 DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-""}
 STORAGE_PREFIX=${STORAGE_PREFIX:-/storage}
+BRANCH_TESTSUITE="${BRANCH_TESTSUITE:-develop}"
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
   REGISTRY_PREFIX=${DOCKER_REGISTRY_HOST}/
@@ -59,7 +60,7 @@ deployment_name=`docker inspect -f "{{ .Name }}" $deploy_id|cut -c2-`
 testsuite_name="ts-linked-to-$deployment_name"
 
 # run StoRM testsuite when deployment is over
-docker run --link $deployment_name:docker-storm.cnaf.infn.it \
+docker run -e "BRANCH_TESTSUITE=${BRANCH_TESTSUITE}" --link $deployment_name:docker-storm.cnaf.infn.it \
   -v /etc/localtime:/etc/localtime:ro \
   --name $testsuite_name \
   ${REGISTRY_PREFIX}italiangrid/storm-testsuite
