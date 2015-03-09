cac3c7cb9dba365cc57df2821bbeb8775e27fe74
dandreotti <daniele.andreotti@cnaf.infn.it>
2015-03-09 12:18:45 +0100

    Changed the name of the parameter used for selecting a specific branch of the storm testsuite

--- docker/storm-deployment-test/server.sh
+++ docker/storm-deployment-test/server.sh
@@ -26,7 +26,7 @@ PLATFORM="${PLATFORM:-SL6}"
 STORM_REPO="${STORM_REPO:-http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_storm_develop_SL6/lastSuccessfulBuild/artifact/storm_develop_sl6.repo}"
 DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-""}
 STORAGE_PREFIX=${STORAGE_PREFIX:-/storage}
-BRANCH_TESTSUITE="${BRANCH_TESTSUITE:-develop}"
+TESTSUITE_BRANCH="${TESTSUITE_BRANCH:-develop}"
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
   REGISTRY_PREFIX=${DOCKER_REGISTRY_HOST}/
@@ -60,7 +60,7 @@ deployment_name=`docker inspect -f "{{ .Name }}" $deploy_id|cut -c2-`
 testsuite_name="ts-linked-to-$deployment_name"
 
 # run StoRM testsuite when deployment is over
-docker run -e "BRANCH_TESTSUITE=${BRANCH_TESTSUITE}" --link $deployment_name:docker-storm.cnaf.infn.it \
+docker run -e "TESTSUITE_BRANCH=${TESTSUITE_BRANCH}" --link $deployment_name:docker-storm.cnaf.infn.it \
   -v /etc/localtime:/etc/localtime:ro \
   --name $testsuite_name \
   ${REGISTRY_PREFIX}italiangrid/storm-testsuite
